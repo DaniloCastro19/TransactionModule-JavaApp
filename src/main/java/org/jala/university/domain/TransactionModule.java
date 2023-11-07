2284ada4d1193446afaf6c2116b013b153662d9b
Angel Favio Calvi Cavero <favio.calvi@jala.university>
2023-11-07 12:45:28 -0500

    TM-010 TransactionModule and TransactionModuleImpl refactoring, and TransactionService changes

--- src/main/java/org/jala/university/domain/TransactionModule.java
+++ src/main/java/org/jala/university/domain/TransactionModule.java
@@ -1,12 +1,5 @@
 package org.jala.university.domain;
 
-import org.jala.university.model.BankUser;
-import org.jala.university.model.Transaction;
-import org.jala.university.model.Account;
-
-import java.util.List;
-import java.util.UUID;
-
 public interface TransactionModule {
     void depositTransaction(Transaction transaction);
 
