2a669e4606c6d9a0649337ab880f5636798d4b71
Danilo Andrés Castro De la hoz <danilo.castro@jala.university>
2023-11-23 23:53:56 +0000

    TM-025: Transation Form BugFix.

--- src/main/java/org/jala/university/domain/TransactionService.java
+++ src/main/java/org/jala/university/domain/TransactionService.java
@@ -1,18 +1,14 @@
 package org.jala.university.domain;
 
 import jakarta.transaction.Transactional;
-import org.jala.university.model.Currency;
-import org.jala.university.model.Account;
-import org.jala.university.model.Transaction;
-import org.jala.university.model.TransactionType;
-import org.jala.university.model.TransactionStatus;
-import org.jala.university.model.BankUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
+import org.jala.university.model.Account;
+import org.jala.university.model.BankUser;
+import org.jala.university.model.Transaction;
+import org.jala.university.model.TransactionStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
@@ -29,82 +25,61 @@ public class TransactionService {
     }
 
     /**
-     * Processes a deposit transaction, transferring an amount from a source account to a recipient account.
-     * It creates a new transaction record with a DEPOSIT type, updates the source account balance,
-     * and saves the transaction to the database.
-     *
-     * @param recipientAccountNumber the unique identifier of the recipient account to which funds are to be deposited
-     * @param amount             the amount of money to deposit
-     * @param description        a description of the transaction for record-keeping purposes
-     * @param currency           the currency in which the deposit is made
-     * @param date               the date when the transaction is to be executed
+     * Processes a deposit transaction, deducting an amount from a source account.
+     * @param transaction  the transation builded to be processed in the method.
+     * @return The transaction carried out
      */
+
     @Transactional
-    public void deposit(String recipientAccountNumber, Long amount, String description, Currency currency, Date date) {
-        List<BankUser> recipientUserResults = userModule.findUsersByAccountNumber(recipientAccountNumber);
-        Account recipientAccount = recipientUserResults.get(0).getAccount();
+    public Transaction deposit(Transaction transaction) {
+        if (transaction.getAmount() <= 0) {
+            transaction.setStatus(TransactionStatus.FAILED);
+        }else {
+            List<BankUser> accountToUserResults = userModule.findUsersByAccountNumber(transaction.getAccountTo().getAccountNumber());
 
+            Account accountTo = accountToUserResults.get(0).getAccount();
+            Long amount = transaction.getAmount();
 
-        recipientAccount.setBalance(recipientAccount.getBalance() + amount);
-
-        Transaction depositTransaction = Transaction.builder()
-                .id(UUID.randomUUID())
-                .date(date)
-                .type(TransactionType.DEPOSIT)
-                .amount(amount)
-                .currency(currency)
-                .accountTo(recipientAccount)
-                .status(TransactionStatus.COMPLETED)
-                .description(description)
-                .build();
-
-        transactionModule.createTransaction(depositTransaction);
+            Long accountToNewBalance = accountTo.getBalance() + amount;
+            accountTo.setBalance(accountToNewBalance);
+            System.out.println("Balance de " + accountTo.getOwner().getFirstName() + "= " + accountTo.getBalance());
+            transaction.setStatus(TransactionStatus.COMPLETED);
+        }
+        transactionModule.createTransaction(transaction);
+        return transaction;
     }
 
     /**
      * Processes a withdrawal transaction, deducting an amount from a source account.
-     *
-     * @param sourceAccountNumber  the unique identifier of the source account
-     * @param amount           the amount to withdraw
-     * @return true if the withdrawal was successful, false if there were insufficient funds
+     * @param transaction  the transation builded to be processed in the method.
+     * @return The transaction carried out
      */
     @Transactional
-    public boolean withdrawal(String sourceAccountNumber, Long amount, String description, Currency currency, Date date) {
-        List<BankUser> sourceUserResults = userModule.findUsersByAccountNumber(sourceAccountNumber);
-        Account sourceAccount = sourceUserResults.get(0).getAccount();
-
-        if (sourceAccount.getBalance() < amount) {
-            return false;
+    public Transaction withdrawal(Transaction transaction) {
+        if (transaction.getAmount() <= 0 || transaction.getAmount() > transaction.getAccountFrom().getBalance()) {
+            transaction.setStatus(TransactionStatus.FAILED);
+        }else {
+            List<BankUser> accountFromUserResults = userModule.findUsersByAccountNumber(transaction.getAccountFrom().getAccountNumber());
+            Account accountFrom = accountFromUserResults.get(0).getAccount();
+            Long accountFromNewBalance = accountFrom.getBalance() - transaction.getAmount();
+            accountFrom.setBalance(accountFromNewBalance);
+            System.out.println("Balance de " + accountFrom.getOwner().getFirstName() + "= " + accountFrom.getBalance());
+            transaction.setStatus(TransactionStatus.COMPLETED);
         }
-
-        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
-        userModule.update(sourceAccount);
-
-        Transaction withdrawalTransaction = Transaction.builder()
-                .id(UUID.randomUUID())
-                .date(date)
-                .type(TransactionType.WITHDRAWAL)
-                .amount(amount)
-                .currency(currency)
-                .accountFrom(sourceAccount)
-                .status(TransactionStatus.COMPLETED)
-                .description(description)
-                .build();
-
-        transactionModule.createTransaction(withdrawalTransaction);
-        return true;
+        transactionModule.createTransaction(transaction);
+        return transaction;
     }
     /**
      * Processes a transfer transaction.
      *
      * @param transaction  the transation builded to be is processed in the method.
+     * @return the transaction carried out.
      */
+
     @Transactional
-    public TransactionStatus transfer(Transaction transaction){
+    public Transaction transfer(Transaction transaction){
         if (transaction.getAmount() <= 0 || transaction.getAmount() > transaction.getAccountFrom().getBalance()) {
             transaction.setStatus(TransactionStatus.FAILED);
-            transactionModule.createTransaction(transaction);
-            return TransactionStatus.FAILED;
         } else {
             List<BankUser> accountFromUserResults = userModule.findUsersByAccountNumber(transaction.getAccountFrom().getAccountNumber());
             List<BankUser> accountToUserResults = userModule.findUsersByAccountNumber(transaction.getAccountTo().getAccountNumber());
@@ -119,9 +94,10 @@ public class TransactionService {
             accountTo.setBalance(accountToNewBalance);
             System.out.println("Balance de " + accountFrom.getOwner().getFirstName() + "= " + accountFrom.getBalance());
             System.out.println("Balance de " + accountTo.getOwner().getFirstName() + "= " + accountTo.getBalance());
-            transactionModule.createTransaction(transaction);
-            return TransactionStatus.COMPLETED;
+            transaction.setStatus(TransactionStatus.COMPLETED);
         }
+        transactionModule.createTransaction(transaction);
+        return transaction;
     }
 
     public TransactionStatus scheduledTransfer(ScheduledTransferModel scheduledTransferModel){
